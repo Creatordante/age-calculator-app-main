1ef5361fea686dbc4d1cd0655ec9b77a84bb182d
Creatordante <111818541+Creatordante@users.noreply.github.com>
2023-05-05 15:49:45 -0500

    validation function completed

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-const formContainer = document.querySelector(".form-container");
 const btn = document.getElementById("btn");
 const txtDisplayed = document.querySelector(".txt-container");
 const userTxt = document.querySelectorAll(".input-container");
@@ -11,16 +10,6 @@ const txtYear = document.getElementById("year");
 
 // Event Listeners
 
-formContainer.addEventListener("click", e => {
-
-    // Checks if its an input
-    if(e.target.parentElement.className === "input-container"){
-    }
-
-    
-});
-
-// Summit btn listener
 btn.addEventListener("click", e =>{
     e.preventDefault();
 
@@ -33,16 +22,19 @@ btn.addEventListener("click", e =>{
 
 function validateform(){
 
+    // if all the information is valid Should be 0
+    let validInputs = 0;
+
     // Current date
     const currentDate = new Date();
 
     //User input data
     const day = parseInt(txtDay.value) ;
-    const month = parseInt(txtMonth.value) ;
+    const month = parseInt(txtMonth.value);
     const year = parseInt(txtYear.value);
 
     //Date From user
-    const date = new Date( year , (month - 1) , day) ;
+    const date = new Date( year , (month - 1) , day);
 
     // Reset styles
     cleanHtml();
@@ -52,36 +44,41 @@ function validateform(){
 
         // Check if its empty or diferent from a number
         if(isNaN(parseInt(userInput.lastElementChild.value))){
+            validInputs += 1;
             errorMsg(userInput,"This field is required");
             return;
         }
-
         //Check for invalid numbers in the date
-        if(userInput.lastElementChild.id === "day" ){
+        else if(userInput.lastElementChild.id === "day" ){
             if(parseInt(userInput.lastElementChild.value) > 31){
+                validInputs += 1;
                 errorMsg(userInput,"Must be a valid day");
                 return;
             }
             else if (parseInt(userInput.lastElementChild.value) !== date.getDate()){
+                validInputs += 1;
                 errorMsg(userInput,"Must be a valid date");
-                return;
             }
         }
         else if (userInput.lastElementChild.id === "month" && parseInt(userInput.lastElementChild.value) > 12){
+            validInputs += 1;
             errorMsg(userInput,"Must be a valid month");
-            return;
-                
+            return;     
         }
-        else if (userInput.lastElementChild.id === "year" && parseInt(userInput.lastElementChild.value) > currentDate.getFullYear()){
+        else if (userInput.lastElementChild.id === "year" && parseInt(userInput.lastElementChild.value) > currentDate.getFullYear() ){
+            validInputs += 1;
             errorMsg(userInput,"Must be in the past");
             return;
         }
-
     });
+
+    if(validInputs === 0){
+        console.log("All valid");
+    }
 }
 
 function errorMsg(userInput,msg){
-
+    
     //Creates element
     const errorMsg = document.createElement("P");
     errorMsg.innerText = msg;
@@ -96,6 +93,13 @@ function errorMsg(userInput,msg){
 }
 
 function cleanHtml(){
-    
+
+    userTxt.forEach( el =>{
+        if(el.lastElementChild.className === "error-msg"){
+            el.firstElementChild.classList.remove("error");
+            el.firstElementChild.nextElementSibling.classList.remove("error-border");
+            el.removeChild(el.lastElementChild);
+        }
+    });
 }
 
